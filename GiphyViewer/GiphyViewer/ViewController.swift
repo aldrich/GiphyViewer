6991e0062404c1bd33820f261fb1de483b11509a
Aldrich Co <aco@freelancer.com>
2020-02-28 17:25:14 +0800

    Created new project

---
+++ GiphyViewer/GiphyViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphyViewer
+//
+//  Created by Aldrich Co on 2/28/20.
+//  Copyright © 2020 Aldrich Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
