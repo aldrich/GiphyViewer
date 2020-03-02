a6c18a105389ad2e9f9216018a65dd665ac70c18
Aldrich Co <aco@freelancer.com>
2020-03-02 22:42:44 +0800

    Added comments in the GIFCache class

--- GiphyViewer/GiphyViewer/Networking/GIFCache.swift
+++ GiphyViewer/GiphyViewer/Networking/GIFCache.swift
@@ -9,13 +9,20 @@
 import Foundation
 import PINCache
 
+// The same images can be cached as different versions; small is for the
+// width-fixed looping version used in the main list; still is the non-looping
+// version with the same size as the original; and large is the best quality
+// image as originally provided by the uploader.
 enum CacheUnit {
 	case small
 	case still
-	case large // or original
+	case large
 }
 
 extension Date {
+	// Used in generate a Date object at a known time interval "ago" from the
+	// current date, which will mainly be used to specify an oldest date to be
+	// preserved in the cache when it gets trimmed.
 	static func secondsAgo(_ seconds: Int) -> Date? {
 		let calendar = Calendar.current
 		return calendar.date(byAdding: .second,
@@ -56,24 +63,30 @@ class GIFCache {
 }
 
 extension GifObject {
-	
+
+	/// Returns true if the image data, for the given unit, is already found
+	/// within the cache.
 	func isCached(forUnit unit: CacheUnit,
 				  in cache: GIFCache = .shared) -> Bool {
 		let key = cacheKey(forUnit: unit)
 		return cache.hasObjectForKey(key)
 	}
-	
+
+	/// Caches the image data locally for the given unit.
 	func cacheData(_ data: Data, forUnit unit: CacheUnit,
 				   in cache: GIFCache = .shared) {
 		let key = cacheKey(forUnit: unit)
 		cache.setObject(data: data, forKey: key)
 	}
-	
+
+	/// Returns the cached image object (Data) if found, for the correct unit,
+	/// otherwise, will return nil.
 	func cachedData(forUnit unit: CacheUnit, in cache: GIFCache = .shared) -> Data? {
 		let key = cacheKey(forUnit: unit)
 		return cache.object(forKey: key)
 	}
-	
+
+	/// The cache key used for a GifObject based on the desired (size) unit.
 	func cacheKey(forUnit unit: CacheUnit) -> String {
 		switch unit {
 		case .small:
